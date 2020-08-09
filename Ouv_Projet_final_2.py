9b9163e264cc0529d666fb6300a99b5a21750937
fareanor3 <61111238+fareanor3@users.noreply.github.com>
2020-08-09 11:27:12 +0200

    multiple changment

--- Ouv_Projet_final_2.py
+++ Ouv_Projet_final_2.py
@@ -23,32 +23,31 @@ def clic_8():
 def clic_9():
     ouv_fen(False,9)
 
-fen1=Tk()
-
 def ouv_fen(Aléatoire,NbreL): # fait passer les informations entre deux programes
     fen1.destroy()
     import Projet_final
     Projet_final.initialiser (Aléatoire,NbreL)
 
+fen1=Tk()
 Can=Canvas(fen1,width=225, height=500, bg='white')
 ImMotus = PhotoImage (file="2.png")
-item=Can.create_image(112,250,image=ImMotus)
+item = Can.create_image(112,250,image=ImMotus)
 Can.place(x=200,y=150)
-Motus=Label(fen1, text="MOTUS", font=font.Font(family="Consolas",size=100))
+Motus = Label(fen1, text="MOTUS", font=font.Font(family="Consolas",size=100))
 Motus.place(x=0,y=0, width=450, height=150)
-Cinq=Button(fen1, text="Mots de 5 lettres",command=clic_5)
+Cinq = Button(fen1, text="Mots de 5 lettres",command=clic_5)
 Cinq.place(x=0,y=150, width=200, height=75)
-Six=Button(fen1, text="Mots de 6 lettres",command=clic_6)
+Six = Button(fen1, text="Mots de 6 lettres",command=clic_6)
 Six.place(x=0,y=225,width=200, height=75)
-Sept=Button(fen1, text="Mots de 7 lettres",command=clic_7)
+Sept = Button(fen1, text="Mots de 7 lettres",command=clic_7)
 Sept.place(x=0,y=300,width=200, height=75)
-Huit=Button(fen1, text="Mots de 8 lettres",command=clic_8)
+Huit = Button(fen1, text="Mots de 8 lettres",command=clic_8)
 Huit.place(x=0,y=375,width=200, height=75)
-Neuf=Button(fen1, text="Mots de 9 lettres",command=clic_9)
+Neuf = Button(fen1, text="Mots de 9 lettres",command=clic_9)
 Neuf.place(x=0,y=450,width=200, height=75)
-BoutonAléatoire=Button(fen1, text="Nombre de lettres aléatoires",command=clic_Aléatoire)
+BoutonAléatoire = Button(fen1, text="Nombre de lettres aléatoires",command=clic_Aléatoire)
 BoutonAléatoire.place(x=0,y=525,width=200, height=75)
-Quit=Button(fen1, text="Quitter",command=fen1.quit)
+Quit = Button(fen1, text="Quitter",command=fen1.quit)
 Quit.place(x=0,y=600,width=200, height=50)
 
 fen1.geometry("425x650")
--- Projet_final.py
+++ Projet_final.py
@@ -16,7 +16,7 @@ nb_max_essais = 11
 nb_essais = 0
 NmLi=11
 ROUGE = "ROUGE"
-txt_bnv="Bonjour ! \n Bienvenue dans MOTUS !\n\nLe but ? \n Trouver le plus de mots en 10 mins \n en ayant 11 essais par mot.\n\nVoici le CODE COULEUR : \n ROUGE = Bien placée \n JAUNE = Mal placée \n NOIR = Absente \n\nBon jeu !\n\n"
+txt_bnv="Bonjour ! \n Bienvenue dans MOTUS !\n\n Le but ? \n Trouver le plus de mots en 10 mins \n en ayant 11 essais par mot.\n\nVoici le CODE COULEUR : \n ROUGE = Bien placée \n JAUNE = Mal placée \n NOIR = Absente \n\nBon jeu !\n\n"
 
 hauteur = 30 # hauteur d'une lettre
 largeur = 20 # largeur d'une lettre
@@ -33,18 +33,18 @@ flags = []
 fen=Tk() 
 fen.title('MOTUS')  # titre de la fenetre
 fen.iconbitmap("favicon.ico") # vignette motus 
-Can=Canvas(fen,width=900, height=200, bg='white')
+Can = Canvas(fen,width=900, height=200, bg='white')
 ImMotus = PhotoImage (file="Nm2.png") # rechercher pq il faut stoquer l'image dans une variable avant de las stoque dans item
-item=Can.create_image(450,100,image=ImMotus)
+item = Can.create_image(450,100,image=ImMotus)
 Can.grid(row=0)
 
 def commencer_mot ():
     global mot_a_trouver,mots,flags,NmLi #pour que les nouvelles valeurs soient accessibles ailleurs
     mot_a_trouver = random.choice(mots_possibles)
-    position_indice=random.randint(0,NbreL-1)
+    position_indice = random.randint(0,NbreL-1)
     lettre_indice=mot_a_trouver[position_indice]
-    print(mot_a_trouver)
-    NmLi=11
+    print(mot_a_trouver) # a supprimer si on veux pas la solution en bas
+    NmLi = 11
     svNbreE.set(str(NmLi))
     svNbrL.set("Le mot à trouver a "+str(NbreL)+" lettres.")
     svindice.set("INDICE : "+"_ "*(position_indice)+(lettre_indice)+" "+"_ "*(NbreL-position_indice-1)) #affichage indice
@@ -64,9 +64,9 @@ def initialiser(aleatoire, nombre):
 def commencer_partie():
     global score, NbreL, mots_possibles
     score=0
-    Val["state"]=NORMAL
-    boutondemarrer['state']=DISABLED  # block le reset du mot + timer
-    lbl["text"]= temps
+    Val["state"] = NORMAL
+    boutondemarrer['state'] = DISABLED  # block le reset du mot + timer
+    lbl["text"] = temps
     svStatut.set(txt_bnv)
     if Aléatoire :
         NbreL=random.randint(5,9)
@@ -75,11 +75,11 @@ def commencer_partie():
     fichier_mot.close()
     svNbreE.set(str(NmLi))
     lalargeur=400/NbreL
-    Lettre1.place(x=200,y=200, width=lalargeur, height=50)
-    Lettre2.place(x=200+lalargeur,y=200, width=lalargeur, height=50)
-    Lettre3.place(x=200+(lalargeur*2),y=200, width=lalargeur, height=50)
-    Lettre4.place(x=200+(lalargeur*3),y=200, width=lalargeur, height=50)
-    Lettre5.place(x=200+(lalargeur*4),y=200, width=lalargeur, height=50)
+    Lettre1.place(x=200,y=200,width=lalargeur,height=50)
+    Lettre2.place(x=200+lalargeur,y=200, width=lalargeur,height=50)
+    Lettre3.place(x=200+(lalargeur*2),y=200,width=lalargeur,height=50)
+    Lettre4.place(x=200+(lalargeur*3),y=200,width=lalargeur,height=50)
+    Lettre5.place(x=200+(lalargeur*4),y=200,width=lalargeur,height=50)
 
     if NbreL>=6 :
         Lettre6.place(x=200+(lalargeur*5),y=200, width=lalargeur, height=50)
@@ -106,15 +106,14 @@ def Afficher_les_differences(proposition_joueur,mot_a_trouver):
        if proposition_joueur[i] == mot_a_trouver[i]:
         liste_tags[i]= 'B'
         lettres_restantes.remove(mot_a_trouver[i])
-        #print (liste_tags)
 
 ### on cherche ensuite les lettres mal placees ###
     
     for i in range (len(proposition_joueur)):
-            if liste_tags[i] != 'B':
-                if proposition_joueur[i] in lettres_restantes:
-                    liste_tags[i]= 'M'
-                    lettres_restantes.remove(proposition_joueur[i])
+        if liste_tags[i] != 'B':
+            if proposition_joueur[i] in lettres_restantes:
+                liste_tags[i]= 'M'
+                lettres_restantes.remove(proposition_joueur[i])
     return liste_tags # return pour stocker
 
 def entree_mot() :
@@ -137,7 +136,7 @@ def entree_mot() :
         NmLi=NmLi+1
         return
 
-    if le_mot not in mots_possibles: # permet aussi de bloquer les chiffres dans les chaines de caractères.
+    if le_mot not in mots_possibles: # permet aussi de bloquer les chiffres dans les chaines de caracteres.
         svStatut.set("⚠ Le mot n'est pas dans le dictionnaire.\n\n""Merci de réessayer")
         NmLi=NmLi+1
         return
@@ -159,7 +158,6 @@ def entree_mot() :
     for m in range(11):
         for l in range(NbreL):
             var_lettres[m][l].set(mots[m][l]) # a quoi sert de mettre d'indice "m" et "l" ?
-
             couleur = "black" # couleur inicialisation
 
             if flags[m][l] == "B":
@@ -169,6 +167,8 @@ def entree_mot() :
 
             labels_lettres[m][l].configure(fg=couleur)
     svNbreE.set(str(NmLi))
+
+# demander a thomas de me reexpliquer cette double boucle. pq dans une boucle ?
 
 for m in range(11): # m est le numéro de la ligne qu'on créé
     ligne_lettres = [] # va stocker tous les string var d'une seule ligne
@@ -181,17 +181,15 @@ for m in range(11): # m est le numéro de la ligne qu'on créé
         label.place(x= position_x + l*largeur, y=position_y + m*hauteur) # place au bonne endroit sur l'écran
         ligne_lettres.append(sv)
         ligne_labels.append(label)
-    
     # on ajoute la ligne au tableau global
     var_lettres.append(ligne_lettres)
     labels_lettres.append(ligne_labels)
 
-def decompte(): # pour le timer chono
-    time=int(lbl["text"])
+def decompte(): # timer
+    time = int(lbl["text"])
     if time > 0:
-        lbl["text"]="%03d" %(time -1) # trois chiffres affichés en parmanance, sinon %02 pour 2 chiffres
+        lbl["text"]="%03d" %(time-1) # trois chiffres affichés en parmanance, sinon %02 pour 2 chiffres
         fen.after(1000,decompte) # en mili-seconde d'où 1000 ms = 1s
-
     elif time == 0 :
         svStatut.set("Partie terminée.\nTu as dépassé le temps accordé, le mot était "+str(mot_a_trouver)+".\nTon score est donc de "+str(score)+" point(s).\n\nRecommence !\n\n\n\n\n\n↓")
         boutondemarrer['state']=NORMAL
@@ -202,26 +200,26 @@ def jouer_son():
         import winsound
         winsound.PlaySound("motus.wav", winsound.SND_FILENAME | winsound.SND_ASYNC) # joue le son pendant que le code s'execute, et donc sans le stop de 1 seconde. (dis asynchrone) - FILENAME = fichier - | = melange les deux infos en une variable unique
 
-Val=Button(fen, text='VALIDER', command=entree_mot, width=30, height=40,font=font.Font(family="Consolas",size=12)) #bouton valider
+Val=Button(fen, text='VALIDER', command = entree_mot, width=30, height=40,font=font.Font(family="Consolas",size=12)) #bouton valider
 Val.place(x=600,y=200, width=120, height=50)
-Quit=Button(fen,text='QUITTER', command=fen.destroy, font=font.Font(family="Consolas",size=12),relief="raised")
+Quit=Button(fen,text = 'QUITTER', command = fen.destroy, font=font.Font(family="Consolas",size=12),relief="raised")
 Quit.place(x=720,y=548, width=180, height=52)
-Val["state"]=DISABLED
-lblNbreE=Label(fen,text= "Nombre d'essais restants :\n\n\n\n\n\n\n\n\n",font=font.Font(family="Consolas",size=8),relief="solid")
+Val["state"] = DISABLED
+lblNbreE = Label(fen,text= "Nombre d'essais restants :\n\n\n\n\n\n\n\n\n",font=font.Font(family="Consolas",size=8),relief="solid")
 lblNbreE.place(x=720,y=298, width=180, height=175)
 svNbreE = StringVar()
-NbreE=Label(fen,textvariable=svNbreE,font=font.Font(family="Consolas",size=50))
+NbreE = Label(fen,textvariable=svNbreE,font=font.Font(family="Consolas",size=50))
 NbreE.place(x=770,y=350,width=75, height=75)
 temps = 600
-lbl=Label(fen, text = temps, fg = '#AA0000', width = 5, font = "Time 50 bold",relief="solid")
+lbl = Label(fen, text = temps, fg = '#AA0000', width = 5, font = "Time 50 bold",relief="solid")
 lbl.place(x=720,y=200,width=180,height=100)
 svNbrL= StringVar()
-LblNbrL=Label(fen, textvariable=svNbrL, relief="solid")
+LblNbrL = Label(fen, textvariable=svNbrL, relief="solid")
 LblNbrL.place(x=0,y=248,width=200, height=50)
 svStatut = StringVar()
-Labelstatut = Label(fen, textvariable=svStatut, width=31,wraplength=220, relief="solid") #31 caractère, 225 pixels ( dans la police de base environ = à 8 pixels; donc difficile d'augmenter la police générale)
-Labelstatut.place(x=0, y=296, width=200,height=304) #text commentaires/statut
-boutondemarrer=Button(fen, text='COMMENCER',command=commencer_partie, font=font.Font(family="Consolas",size =15),relief="raised") #bouton demarer
+Labelstatut = Label(fen, textvariable=svStatut, width=31,wraplength=220, relief="solid") # 31 caractère, 225 pixels ( dans la police de base environ = à 8 pixels; donc difficile d'augmenter la police générale)
+Labelstatut.place(x=0, y=296, width=200,height=304) # text commentaires/statut
+boutondemarrer = Button(fen, text='COMMENCER',command=commencer_partie, font=font.Font(family="Consolas",size =15),relief="raised") # bouton demarer
 boutondemarrer.place(x=720, y=473, width=180, height=75)
 svindice = StringVar()
 svindice.set("INDICE : ")
@@ -244,18 +242,16 @@ Mot7.set("")
 Mot8=StringVar()
 Mot8.set("")
 Mot9=StringVar()
-Mot9.set("")         # optimiser en faisant un tableau de mots et de lettes si possible
-
-
-Lettre9=Entry(fen,bg='white',textvariable=Mot9,font=font.Font(family="Consolas",size=20))
-Lettre8=Entry(fen,bg='white',textvariable=Mot8,font=font.Font(family="Consolas",size=20))
-Lettre7=Entry(fen,bg='white',textvariable=Mot7,font=font.Font(family="Consolas",size=20))
-Lettre6=Entry(fen,bg='white',textvariable=Mot6,font=font.Font(family="Consolas",size=20))
-Lettre5=Entry(fen,bg='white',textvariable=Mot5,font=font.Font(family="Consolas",size=20))
-Lettre4=Entry(fen,bg='white',textvariable=Mot4,font=font.Font(family="Consolas",size=20))
-Lettre3=Entry(fen,bg='white',textvariable=Mot3,font=font.Font(family="Consolas",size=20))
-Lettre2=Entry(fen,bg='white',textvariable=Mot2,font=font.Font(family="Consolas",size=20))
-Lettre1=Entry(fen,bg='white',textvariable=Mot1,font=font.Font(family="Consolas",size=20))
+Mot9.set("")         # optimiser en faisant un tableau de mots et de lettes
+Lettre9 = Entry(fen,bg='white',textvariable=Mot9,font=font.Font(family="Consolas",size=20))
+Lettre8 = Entry(fen,bg='white',textvariable=Mot8,font=font.Font(family="Consolas",size=20))
+Lettre7 = Entry(fen,bg='white',textvariable=Mot7,font=font.Font(family="Consolas",size=20))
+Lettre6 = Entry(fen,bg='white',textvariable=Mot6,font=font.Font(family="Consolas",size=20))
+Lettre5 = Entry(fen,bg='white',textvariable=Mot5,font=font.Font(family="Consolas",size=20))
+Lettre4 = Entry(fen,bg='white',textvariable=Mot4,font=font.Font(family="Consolas",size=20))
+Lettre3 = Entry(fen,bg='white',textvariable=Mot3,font=font.Font(family="Consolas",size=20))
+Lettre2 = Entry(fen,bg='white',textvariable=Mot2,font=font.Font(family="Consolas",size=20))
+Lettre1 = Entry(fen,bg='white',textvariable=Mot1,font=font.Font(family="Consolas",size=20))
 
 
 ### patch notes : au lieu d'avoir n lettre => faire un tableau et mettre les lettre dedans, idem pour mots avec des string vars ###
